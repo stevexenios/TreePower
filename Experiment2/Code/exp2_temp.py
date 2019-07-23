1ab9d3c7d5a42e1976761a38c6cd7024ad8bf358
NickPurcell <nick.purcell117@gmail.com>
2019-07-22 20:04:59 -0700

    Experiment 2 Code

---
+++ Experiment2/Code/exp2_temp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Nick Purcell
+# UWT 2019
+# Read temp sensors from ADC and save as csv
+
+import csv
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+import time
+from datetime import datetime
+from time import sleep
+
+startTime = time.time()									# Instantiate time
+
+# Instantiate SPI
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+# Save Data for 30 Seconds
+with open('data.csv', 'w+', newline='') as f:           # Open CV file
+        writedat = csv.writer(f)
+        while (time.time() < startTime + 30):
+            for i in range(8):
+                values[i] = mcp.read_adc(i) * 3.3 / 1024             # Get data from ADC
+            writedat.writerow([values[0], values[1], values[2], values[3],
+                               values[4], values[5], values[6], values[7],
+                               datetime.now().isoformat()])     # Write data and time to CSV
+            sleep( 5 )                                  # Sleep for 5 seconds
+os.system("sudo shutdown -h now")						# Shutdown the pi
---
+++ Experiment2/Code/exp2_volt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# Nick Purcell
+# UWT 2019
+# Read peltier voltage from ADC and save as csv
+
+import csv
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+import time
+from datetime import datetime
+from time import sleep
+
+startTime = time.time()									# Instantiate time
+
+# Instantiate software SPI
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Save Data for 30 Seconds
+with open('volt_data.csv', 'w+', newline='') as f:           # Open CV file
+        writedat = csv.writer(f)
+        while (time.time() < startTime + 30):
+            for i in range(8):
+                values[i] = mcp.read_adc(i)             # Get data from ADC
+            values[0] = values[0] * M0 / 1024
+            values[1] = values[1] * M1 / 1024
+            values[2] = values[2] * M2 / 1024
+            values[3] = values[3] * M3 / 1024
+            values[4] = values[4] * M4 / 1024           # CHANGE THE Ms after experimenting with TEGs
+            values[5] = values[5] * M5 / 1024
+            values[6] = values[6] * M6 / 1024
+            values[7] = values[7] * M7 / 1024
+            writedat.writerow([values[0], values[1], values[2],
+                               values[3], datetime.now().isoformat()])     # Write data and time to CSV
+            sleep( 5 )                                  # Sleep for 5 seconds
+os.system("sudo shutdown -h now")						# Shutdown the pi
