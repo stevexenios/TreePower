18d76cf48d8e6d844e24f7d6a2a947d3cd9de8a7
NickPurcell <nick.purcell117@gmail.com>
2019-08-14 18:09:33 -0700

    Jpegification

--- SensorCode/writevolt.py
+++ SensorCode/writevolt.py
@@ -4,15 +4,10 @@
 # UWT 2019
 # Takes peltier voltage data from arduino and saves it to a txt file.
 
-import serial
 from datetime import datetime
 from time import sleep
 
-					  # Give eveything 2 minutes to turn on
-
-ser = serial.Serial('/dev/ttyUSB0', 115200)		  #Instatiate connection to Arduino
-
-fname = "/home/pi/VoltageData" + datetime.now().isoformat() + ".txt" #Instantiate time
+time = datetime.now().isoformat()									#Instantiate time
 
 f = open(fname, "w+")
 f.close()
@@ -22,9 +17,9 @@ while True:
 		if(ser.in_waiting > 0):
 			log = open(fname,"a")
 			ctime = datetime.now().isoformat()
-			line = ser.readline().decode() 		  #Decode data from Arduino
-			log.write(ctime)				  #Write date and time
-			log.write(" (mV) " + line)		  #Write arduino Data
+			line = ser.readline().decode() 		  	#Decode data from Arduino
+			log.write(ctime)				  		#Write date and time
+			log.write(" (mV) " + line)		  		#Write arduino Data
 			print(ctime + " " + line)
 			log.close()
 			ser.reset_input_buffer()
